dd69bdd687470c37e34d662c9083d24bd29b90b2
Paul Ciuraru <ciuraru.paul10@gmail.com>
2024-08-17 20:38:54 +0300

    create header and footer with basic styles

--- client/src/ui/Footer.jsx
+++ client/src/ui/Footer.jsx
@@ -1,5 +1,26 @@
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaRegCopyright } from "react-icons/fa6";
+
 function Footer() {
-	return <div>this is the footer</div>;
+	return (
+		<div className="footer">
+			<div className="footer__copyright">
+				<FaRegCopyright className="footer__copyright-icon" />
+				<p className="footer__copyright-text">
+					Built by <strong>Ciuraru Paul</strong>
+				</p>
+			</div>
+
+			<div className="footer__socials">
+				<p className="footer__socials-text">
+					See other projects I made
+				</p>
+				<FaGithub />
+				<FaLinkedin />
+			</div>
+		</div>
+	);
 }
 
 export default Footer;
--- client/src/ui/Header.jsx
+++ client/src/ui/Header.jsx
@@ -1,5 +1,25 @@
+import { GiCampfire } from "react-icons/gi";
+import { HiBars3 } from "react-icons/hi2";
+import { HiUserCircle } from "react-icons/hi2";
+import { HiOutlineGlobeAlt } from "react-icons/hi2";
+
 function Header() {
-	return <div>this is the header</div>;
+	return (
+		<div className="header">
+			<div className="header__logo-box">
+				<GiCampfire className="header__logo-icon" />
+				<p className="header__logo-text">lazy-logs</p>
+			</div>
+
+			<div className="header__user-box">
+				<HiOutlineGlobeAlt className="header__user-options" />
+				<button className="header__user-button">
+					<HiBars3 className="header__user-menu" />{" "}
+					<HiUserCircle className="header__user-account" />
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default Header;
