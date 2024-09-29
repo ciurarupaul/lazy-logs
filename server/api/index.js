625d312d14651df26dc52063f54b8cfd55c33be1
Paul Ciuraru <ciuraru.paul10@gmail.com>
2024-09-29 23:00:33 +0300

    rethink index.js

--- server/api/index.js
+++ server/api/index.js
@@ -1,18 +1,57 @@
-import app from "../app";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
+import mongoose from "mongoose";
+import globalErrorHandler from "../controllers/errorController.js";
+import bookingRouter from "../routes/bookingRoutes.js";
+import listingRouter from "../routes/listingRoutes.js";
+import reviewRouter from "../routes/reviewRoutes.js";
+import userRouter from "../routes/userRoutes.js";
+import wishlistRouter from "../routes/wishlistRouter.js";
+import AppError from "../utils/appError.js";
 
-const DB = process.env.DATABASE.replace(
-	"<password>",
-	process.env.DATABASE_PASSWORD
+const app = express();
+
+app.use(
+	cors({
+		origin: [
+			"https://lazy-logs-server.vercel.app/",
+			"http://localhost:3000",
+			"http://localhost:5173",
+		],
+		methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+		credentials: true,
+	})
 );
 
-let isConnected;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-export const connectToDatabase = async () => {
+// routes
+app.use("/api/bookings", bookingRouter);
+app.use("/api/listings", listingRouter);
+app.use("/api/reviews", reviewRouter);
+app.use("/api/users", userRouter);
+app.use("/api/wishlist", wishlistRouter);
+
+app.all("*", (req, res, next) => {
+	next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
+
+let isConnected = false;
+const connectToDatabase = async () => {
 	if (isConnected) {
 		return;
 	}
 
 	try {
+		const DB = process.env.DATABASE.replace(
+			"<password>",
+			process.env.DATABASE_PASSWORD
+		);
 		await mongoose.connect(DB, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
@@ -25,7 +64,8 @@ export const connectToDatabase = async () => {
 	}
 };
 
-export default async (req, res) => {
+// Vercel Serverless Function
+export default async function handler(req, res) {
 	await connectToDatabase();
 	app(req, res);
-};
+}
