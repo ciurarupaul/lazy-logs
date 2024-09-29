397eac2300f251767daa1a34e0974ae3a9169e96
Paul Ciuraru <ciuraru.paul10@gmail.com>
2024-09-29 23:25:27 +0300

    accept defeat

--- server/api/index.js
+++ server/api/index.js
@@ -1,47 +1,8 @@
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import express from "express";
 import mongoose from "mongoose";
-import globalErrorHandler from "../controllers/errorController.js";
-import bookingRouter from "../routes/bookingRoutes.js";
-import listingRouter from "../routes/listingRoutes.js";
-import reviewRouter from "../routes/reviewRoutes.js";
-import userRouter from "../routes/userRoutes.js";
-import wishlistRouter from "../routes/wishlistRouter.js";
-import AppError from "../utils/appError.js";
-
-const app = express();
-
-app.use(
-	cors({
-		origin: [
-			"https://lazy-logs-server.vercel.app/",
-			"http://localhost:3000",
-			"http://localhost:5173",
-		],
-		methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-		credentials: true,
-	})
-);
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-
-// routes
-app.use("/api/bookings", bookingRouter);
-app.use("/api/listings", listingRouter);
-app.use("/api/reviews", reviewRouter);
-app.use("/api/users", userRouter);
-app.use("/api/wishlist", wishlistRouter);
-
-app.all("*", (req, res, next) => {
-	next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
-});
-
-app.use(globalErrorHandler);
+import app from "../app.js";
 
 let isConnected = false;
+
 const connectToDatabase = async () => {
 	if (isConnected) {
 		return;
@@ -64,12 +25,7 @@ const connectToDatabase = async () => {
 	}
 };
 
-// Vercel Serverless Function
-// export default async function handler(req, res) {
-// 	await connectToDatabase();
-// 	app(req, res);
-// }
-
-module.exports = (req, res) => {
-	res.status(200).json({ message: "Hello from Serverless Function!" });
-};
+export default async function handler(req, res) {
+	await connectToDatabase();
+	app(req, res);
+}
