dcba1b9339679747d0b496fec8bfdde0b4cc4ce9
Paul Ciuraru <ciuraru.paul10@gmail.com>
2024-10-06 01:54:45 +0300

    add responsiveness for listings and listing until the 900px breakpoint

--- client/src/ui/layout/Footer.jsx
+++ client/src/ui/layout/Footer.jsx
@@ -1,23 +1,22 @@
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { FaRegCopyright } from "react-icons/fa6";
-import { useNavigate } from "react-router-dom";
 
 function Footer() {
-	const navigate = useNavigate();
-
 	const handleGithubRedirect = () => {
-		window.location.href = "https://github.com/ciurarupaul";
+		window.open("https://github.com/ciurarupaul", "_blank");
 	};
+
 	const handleLinkedInRedirect = () => {
-		window.location.href = "https://www.linkedin.com/in/ciurarupaul/";
+		window.open("https://www.linkedin.com/in/ciurarupaul/", "_blank");
 	};
+
 	return (
 		<footer className="footer">
 			<div className="footer__copyright">
 				<FaRegCopyright className="footer__copyright-icon" />
 				<p className="footer__copyright-text">
-					Built by <span>Ciuraru Paul</span>
+					2024, Built by <span>Paul CIURARU</span>
 				</p>
 			</div>
 
